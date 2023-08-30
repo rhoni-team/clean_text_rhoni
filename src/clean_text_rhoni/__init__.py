2836d5d1cebe228ea08d95e714b4152a9628fc08
nataliamd11 <nataliamdenk11@gmail.com>
2023-08-30 11:39:02 -0300

    feat: add imports to init file

--- src/clean_text_rhoni/__init__.py
+++ src/clean_text_rhoni/__init__.py
@@ -1,3 +1,7 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("clean_text_rhoni")
+
+# populate package namespace
+from clean_text_rhoni.base_clean_text import BaseCleanText
+from clean_text_rhoni.clean_text_rhoni import clean_text, clean_text_snake_case
